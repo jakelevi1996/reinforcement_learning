a0fce4ba0aad0e589789a9c9a510b96f964e79c5
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-04 10:52:29 +0100

    Add _rng attribute to KArmedBandit class

--- bandit.py
+++ bandit.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 class KArmedBandit:
-    def __init__(self, k=10):
-        self._action_values = np.random.normal(0, 1, k)
+    def __init__(self, k=10, rng=None):
+        if rng is None:
+            self._rng = np.random.default_rng()
+        else:
+            self._rng = rng
+        self._action_values = self._rng.normal(0, 1, k)
 
     def step(self, action):
         action_value = self._action_values[action]
-        reward = np.random.normal(action_value, 1, 1)
+        reward = self._rng.normal(action_value, 1, 1)
         return reward
 
 class EpsilonGreedy:
