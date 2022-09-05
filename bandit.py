68643c72198efeac32692a5c47103d26e7bc89b0
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-05 09:10:31 +0100

    Add EpsilonGreedyConstantStepSize class, and plot against EpsilonGreedy class on the same axes

--- bandit.py
+++ bandit.py
@@ -59,44 +59,71 @@ class EpsilonGreedy:
             / self._num_action_tries[action]
         )
 
+class EpsilonGreedyConstantStepSize(EpsilonGreedy):
+    def update(self, action, reward):
+        self._value_estimates[action] += (
+            self._step_size * (reward - self._value_estimates[action])
+        )
+
+class AgentResult:
+    def __init__(self, agent_type, num_steps, num_repeats):
+        self.construcor = agent_type
+        self.reward_array = np.zeros([num_repeats, num_steps])
+        self.optimal_choice_array = np.zeros([num_repeats, num_steps])
 
 num_steps = 1000
 num_repeats = 100
-reward_array = np.zeros([num_repeats, num_steps])
-optimal_choice_array = np.zeros([num_repeats, num_steps])
+agent_result_list = [
+    AgentResult(agent_type, num_steps, num_repeats)
+    for agent_type in [EpsilonGreedy, EpsilonGreedyConstantStepSize]
+]
 for i in range(num_repeats):
     if ((i + 1) % 10) == 0:
         print("Performing repeat %i/%i" % (i + 1, num_repeats), end="\r")
     env = KArmedBandit()
-    agent = EpsilonGreedy()
     optimal_actions = [
         a for a, value in enumerate(env._action_values)
         if value == max(env._action_values)
     ]
+    for agent_result in agent_result_list:
+        agent = agent_result.construcor()
 
-    for j in range(num_steps):
-        action = agent.choose_action()
-        reward = env.step(action)
-        agent.update(action, reward)
-        reward_array[i, j] = reward
-        if action in optimal_actions:
-            optimal_choice_array[i, j] = 1
+        for j in range(num_steps):
+            action = agent.choose_action()
+            reward = env.step(action)
+            agent.update(action, reward)
+            agent_result.reward_array[i, j] = reward
+            if action in optimal_actions:
+                agent_result.optimal_choice_array[i, j] = 1
 
 print("\nPlotting results...")
 t = np.arange(num_steps)
 t_tiled = np.tile(t.reshape(1, -1), [num_repeats, 1])
-alpha = 0.5/num_repeats
+line_props = ["b", "-", "", 1, 20]
+marker_props = ["b", "", "o", 0.5 / num_repeats, 10]
+rewards_line_list = [
+    line
+    for a in agent_result_list
+    for line in [
+        plotting.Line(t_tiled, a.reward_array, *marker_props),
+        plotting.Line(t, np.mean(a.reward_array, axis=0), *line_props),
+    ]
+]
+percent_optimal_choice_line_list = [
+    plotting.Line(
+        t,
+        100 * np.mean(agent_result.optimal_choice_array, axis=0),
+        *line_props,
+    )
+    for agent_result in agent_result_list
+]
 plotting.plot(
-    [
-        plotting.Line(t_tiled, reward_array, "b", "", "o", alpha, 10),
-        plotting.Line(t, np.mean(reward_array, axis=0), "b", "-", "", 1, 20),
-    ],
+    rewards_line_list,
     "Epsilon greedy rewards",
     axis_properties=plotting.AxisProperties("Time", "Reward", None, [-2, 4]),
 )
-percent_optimal_choice = 100*np.mean(optimal_choice_array, axis=0)
 plotting.plot(
-    [plotting.Line(t, percent_optimal_choice, "b", "-", "", 1, 20)],
+    percent_optimal_choice_line_list,
     "Epsilon greedy percentage of optimal actions",
     axis_properties=plotting.AxisProperties(
         "Time",
