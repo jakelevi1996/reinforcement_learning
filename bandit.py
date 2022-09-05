d171aaba512a705dd57562b482c0797d481cffdc
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-05 15:53:44 +0100

    Make plot of variance of epsilon-greedy rewards

--- bandit.py
+++ bandit.py
@@ -83,6 +83,10 @@ class AgentResult:
         self.reward_array = np.zeros([num_repeats, num_steps])
         self.optimal_choice_array = np.zeros([num_repeats, num_steps])
 
+    def get_mean_std_reward(self):
+        self.mean_reward = np.mean(self.reward_array, axis=0)
+        self.std_reward = np.std(self.reward_array, axis=0)
+
 num_steps = 1000
 num_repeats = 100
 agent_result_list = [
@@ -114,25 +118,38 @@ mt = 0.5 / num_repeats
 line_props = {"ls": "-", "marker": "", "alpha": 1, "zorder": 20}
 marker_props = {"ls": "", "marker": "o", "alpha": mt, "zorder": 10}
 cp = plotting.ColourPicker(len(agent_result_list))
+for a in agent_result_list:
+    a.get_mean_std_reward()
 rewards_line_list = [
-    line
+    plotting.Line(
+        t,
+        agent_result.reward_array.T,
+        color=cp(i),
+        label="%s (single reward)" % agent_result.name,
+        **marker_props,
+    )
     for i, agent_result in enumerate(agent_result_list)
-    for line in [
-        plotting.Line(
-            t,
-            agent_result.reward_array.T,
-            color=cp(i),
-            label="%s (single reward)" % agent_result.name,
-            **marker_props,
-        ),
-        plotting.Line(
-            t,
-            np.mean(agent_result.reward_array, axis=0),
-            color=cp(i),
-            label="%s (mean reward)" % agent_result.name,
-            **line_props,
-        ),
-    ]
+]
+mean_reward_line_list = [
+    plotting.Line(
+        t,
+        agent_result.mean_reward,
+        color=cp(i),
+        label="%s (mean reward)" % agent_result.name,
+        **line_props,
+    )
+    for i, agent_result in enumerate(agent_result_list)
+]
+std_reward_fb_list = [
+    plotting.FillBetween(
+        t,
+        agent_result.mean_reward + agent_result.std_reward,
+        agent_result.mean_reward - agent_result.std_reward,
+        color=cp(i),
+        label="$\\pm\\sigma$",
+        alpha=0.2,
+    )
+    for i, agent_result in enumerate(agent_result_list)
 ]
 percent_optimal_choice_line_list = [
     plotting.Line(
@@ -145,9 +162,24 @@ percent_optimal_choice_line_list = [
     for i, agent_result in enumerate(agent_result_list)
 ]
 plotting.plot(
-    rewards_line_list,
+    [
+        line
+        for line_pair in zip(rewards_line_list, mean_reward_line_list)
+        for line in line_pair
+    ],
     "Epsilon greedy rewards",
     axis_properties=plotting.AxisProperties("Time", "Reward", None, [-2, 4]),
+    legend_properties=plotting.LegendProperties(0.4),
+    figsize=[12, 6],
+)
+plotting.plot(
+    [
+        line
+        for line_pair in zip(mean_reward_line_list, std_reward_fb_list)
+        for line in line_pair
+    ],
+    "Epsilon greedy rewards (mean and variance)",
+    axis_properties=plotting.AxisProperties("Time", "Reward", None, [-1, 4]),
     legend_properties=plotting.LegendProperties(0.4),
     figsize=[12, 6],
 )
