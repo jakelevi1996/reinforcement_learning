eec9bc88db8838cbdad0ea8e3b06e1486dbc322f
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-06 17:43:27 +0100

    Add option to specify non-mean reward

--- environments/k_armed_bandit.py
+++ environments/k_armed_bandit.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 class KArmedBandit:
-    def __init__(self, k=10, rng=None):
+    def __init__(self, k=10, rng=None, mean_reward=0):
         if rng is None:
             self._rng = np.random.default_rng()
         else:
             self._rng = rng
-        self._action_values = self._rng.normal(size=k)
+        self._action_values = self._rng.normal(loc=mean_reward, size=k)
 
     def step(self, action):
         action_value = self._action_values[action]
