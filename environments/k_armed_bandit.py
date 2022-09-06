8d70858ae2beb594392f5409c8afdac1a6498e51
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-06 16:52:05 +0100

    Don't specify variance unnecessarily (saves >15% running time!?)

--- environments/k_armed_bandit.py
+++ environments/k_armed_bandit.py
@@ -6,9 +6,9 @@ class KArmedBandit:
             self._rng = np.random.default_rng()
         else:
             self._rng = rng
-        self._action_values = self._rng.normal(0, 1, k)
+        self._action_values = self._rng.normal(size=k)
 
     def step(self, action):
         action_value = self._action_values[action]
-        reward = self._rng.normal(action_value, 1, 1)
+        reward = self._rng.normal(action_value)
         return reward
