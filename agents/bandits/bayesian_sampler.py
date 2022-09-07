522d98632ca62b5ff8e3407a34af812739a09978
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-07 21:18:43 +0100

    Rename statistical variables for consistency and clarity

--- agents/bandits/bayesian_sampler.py
+++ agents/bandits/bayesian_sampler.py
@@ -4,14 +4,14 @@ from agents.bandits.bandit_agent import _BanditAgent
 class _BayesianSampler(_BanditAgent):
     def __init__(self, num_actions=10, rng=None):
         self._step = 0
+        self._num_actions = num_actions
+        self._num_action_tries = np.zeros(num_actions, dtype=np.int)
         self._prior_mean = 0
         self._prior_mean_square = 0
         self._prior_var = 0
-        self._num_actions = num_actions
-        self._num_action_tries = np.zeros(num_actions, dtype=np.int)
-        self._mean_reward = np.zeros(num_actions)
-        self._mean_square_reward = np.zeros(num_actions)
-        self._var_reward = np.ones(num_actions)
+        self._likelihood_mean = np.zeros(num_actions)
+        self._likelihood_mean_square = np.zeros(num_actions)
+        self._likelihood_var = np.ones(num_actions)
 
         if rng is None:
             self._rng = np.random.default_rng()
@@ -23,43 +23,46 @@ class _BayesianSampler(_BanditAgent):
         if self._prior_var == 0:
             return self._rng.choice(self._num_actions)
 
-        value_posterior_var = (
+        posterior_var = (
             1.0 / (
-                (self._num_action_tries / self._var_reward)
+                (self._num_action_tries / self._likelihood_var)
                 + (1.0 / self._prior_var)
             )
         )
-        value_posterior_mean = (
+        posterior_mean = (
             (
-                (self._num_action_tries * self._mean_reward / self._var_reward)
+                (
+                    self._num_action_tries * self._likelihood_mean
+                    / self._likelihood_var
+                )
                 + (self._prior_mean / self._prior_var)
             )
-            * value_posterior_var
+            * posterior_var
         )
 
         samples = self._rng.normal(
-            value_posterior_mean,
-            np.sqrt(value_posterior_var),
+            posterior_mean,
+            np.sqrt(posterior_var),
         )
         action = np.argmax(samples)
         return action
 
     def update(self, action, reward):
         self._num_action_tries[action] += 1
-        self._mean_reward[action] += (
-            (reward - self._mean_reward[action])
+        self._likelihood_mean[action] += (
+            (reward - self._likelihood_mean[action])
             / self._num_action_tries[action]
         )
-        self._mean_square_reward[action] += (
-            ((reward * reward) - self._mean_square_reward[action])
+        self._likelihood_mean_square[action] += (
+            ((reward * reward) - self._likelihood_mean_square[action])
             / self._num_action_tries[action]
         )
-        self._var_reward[action] = (
-            self._mean_square_reward[action]
-            - np.square(self._mean_reward[action])
+        self._likelihood_var[action] = (
+            self._likelihood_mean_square[action]
+            - np.square(self._likelihood_mean[action])
         )
-        if self._var_reward[action] == 0:
-            self._var_reward[action] = np.var(self._mean_reward)
+        if self._likelihood_var[action] == 0:
+            self._likelihood_var[action] = np.var(self._likelihood_mean)
 
         self._set_prior(reward)
 
@@ -84,8 +87,8 @@ class BayesianSamplerBroadPrior(_BayesianSampler):
 
 class BayesianSamplerValuePrior(_BayesianSampler):
     def _set_prior(self, reward):
-        self._prior_mean = np.mean(self._mean_reward)
-        self._prior_var = np.var(self._mean_reward)
+        self._prior_mean = np.mean(self._likelihood_mean)
+        self._prior_var = np.var(self._likelihood_mean)
 
     def get_name(self):
         return "Bayesian sampler (value prior)"
