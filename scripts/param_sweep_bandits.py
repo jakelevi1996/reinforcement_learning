33ddda33e87903556cc465f0eeb6ed5197af2de3
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-15 20:33:52 +0100

    Test EpsilonGreedyConstantStepSize agent class step_size is not swept for TestEpsilonGreedy because it doesn't do anything

--- scripts/param_sweep_bandits.py
+++ scripts/param_sweep_bandits.py
@@ -11,14 +11,17 @@ import util
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class TestEpsilonGreedy(sweep.Experiment):
+class _TestBanditAgent(sweep.Experiment):
+    def get_agent(self, **kwargs):
+        raise NotImplementedError()
+
     def __init__(self, num_steps, seeder):
         self._num_steps = num_steps
         self._seeder = seeder
 
-    def run(self, epsilon, step_size):
+    def run(self, **kwargs):
         env = environments.KArmedBandit()
-        agent = agents.bandits.EpsilonGreedy(epsilon, step_size)
+        agent = self.get_agent(**kwargs)
         total_reward = 0
 
         for _ in range(self._num_steps):
@@ -30,12 +33,53 @@ class TestEpsilonGreedy(sweep.Experiment):
         mean_reward = total_reward / self._num_steps
         return mean_reward
 
+class TestEpsilonGreedy(_TestBanditAgent):
+    def get_agent(self, epsilon):
+        return agents.bandits.EpsilonGreedy(epsilon)
+
+class TestEpsilonGreedyConstantStepSize(_TestBanditAgent):
+    def get_agent(self, epsilon, step_size):
+        agent = agents.bandits.EpsilonGreedyConstantStepSize(
+            epsilon,
+            step_size,
+        )
+        return agent
+
 def main(args):
-    test_epsilon_greedy(args)
+    filename_list_list = [
+        test_epsilon_greedy(args),
+        test_epsilon_greedy_constant_step_size(args),
+    ]
+    print("\nPlots saved with the following filenames:\n")
+    for filename_list in filename_list_list:
+        print(*filename_list, sep="\n", end="\n\n")
+
 
 def test_epsilon_greedy(args):
     seeder = util.Seeder()
     experiment = TestEpsilonGreedy(args.num_steps, seeder)
+    param_sweeper = sweep.ParamSweeper(
+        experiment,
+        n_repeats=args.num_repeats,
+        print_every=50,
+    )
+
+    param_sweeper.add_parameter(
+        sweep.Parameter(
+            "epsilon",
+            0.1,
+            val_lo=0.01,
+            val_hi=0.6,
+            log_space=True,
+        )
+    )
+    param_sweeper.find_best_parameters()
+    results_dir = os.path.join(args.results_dir, "Epsilon_greedy")
+    return param_sweeper.plot(results_dir)
+
+def test_epsilon_greedy_constant_step_size(args):
+    seeder = util.Seeder()
+    experiment = TestEpsilonGreedyConstantStepSize(args.num_steps, seeder)
     param_sweeper = sweep.ParamSweeper(
         experiment,
         n_repeats=args.num_repeats,
@@ -61,8 +105,11 @@ def test_epsilon_greedy(args):
         )
     )
     param_sweeper.find_best_parameters()
-    results_dir = os.path.join(args.results_dir, "Epsilon_greedy")
-    param_sweeper.plot(results_dir)
+    results_dir = os.path.join(
+        args.results_dir,
+        "Epsilon_greedy_constant_step_size",
+    )
+    return param_sweeper.plot(results_dir)
 
 if __name__ == "__main__":
 
