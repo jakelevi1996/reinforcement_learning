2f8829d97a0f21932400305e24a93cb17bbdfd6a
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-16 19:09:25 +0100

    Fill in body of test_legend unit test function Add legend to bar plot and reorder tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -37,11 +37,33 @@ def test_plot_fill():
         axis_properties=plotting.AxisProperties(xlabel="x", ylabel="y"),
     )
 
+def test_legend():
+    line1 = plotting.Line([1, 2], [1, 2], marker="o", c="r", label="Red line")
+    line2 = plotting.Line([1.2, 1.8], [1.8, 1.2], c="g", label="Green line")
+    line3 = plotting.Line([1.3, 1.7], [1.5, 1.6], marker="o", c="y")
+    line4 = plotting.HVLine(h=1.7, c="m", ls="--", label="hline")
+    fill1 = plotting.FillBetween(
+        x=[1.3, 1.6],
+        y1=[1.2, 1.3],
+        y2=[1.1, 1.0],
+        fc="b",
+        alpha=0.5,
+        label="Patch",
+    )
+    axis_properties = plotting.AxisProperties(xlabel="x", ylabel="y")
+    plotting.plot(
+        line_list=[line1, line2, line3, line4, fill1],
+        plot_name="test_legend",
+        dir_name=OUTPUT_DIR,
+        axis_properties=axis_properties,
+        legend_properties=plotting.LegendProperties(),
+    )
+
 def test_plot_bar():
     plotting.plot(
         line_list=[
-            plotting.Bar("Red", 3.1, color="r", zorder=10),
-            plotting.Bar("Green", 4.3, color="g", zorder=10),
+            plotting.Bar("Red", 3.1, color="r", zorder=10, label="Bar 1"),
+            plotting.Bar("Green", 4.3, color="g", zorder=10, label="Bar 2"),
         ],
         plot_name="test_plot_bar",
         dir_name=OUTPUT_DIR,
@@ -49,10 +71,8 @@ def test_plot_bar():
             xlabel="Category",
             ylabel="Height",
         ),
+        legend_properties=plotting.LegendProperties(),
     )
-
-def test_legend():
-    pass
 
 def test_log_axes():
     pass
