f6f01c7c5b85ea4eabc0a2d7b3424936ca20b33e
jakelevi1996 <jakelevi@hotmail.co.uk>
2022-09-16 22:34:52 +0100

    Fill in body of test_bandit_environment unit test function

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,2 +1,28 @@
-def test_bandit_environment():
-    pass
+import numpy as np
+import pytest
+import tests.util
+import util
+import environments
+
+OUTPUT_DIR = tests.util.get_output_dir("test_environments")
+
+@pytest.mark.parametrize("repeat", range(3))
+def test_bandit_environment(repeat):
+    printer = util.Printer("%s %i.txt" % ("KArmedBandit", repeat), OUTPUT_DIR)
+    seed = util.Seeder().get_seed("KArmedBandit", repeat)
+    rng = np.random.default_rng(seed)
+    printer.print("Seed = %i" % seed)
+
+    num_actions = rng.choice(range(5, 25))
+    printer.print("num_actions = %i" % num_actions)
+
+    env = environments.KArmedBandit(k=num_actions, rng=rng)
+
+    for action in range(num_actions):
+        reward = env.step(action)
+        printer.print("action = %i, reward = %.1f" % (action, reward))
+
+    for i in range(rng.choice(range(10, 20))):
+        action = rng.choice(num_actions)
+        reward = env.step(action)
+        printer.print("action = %i, reward = %.1f" % (action, reward))
